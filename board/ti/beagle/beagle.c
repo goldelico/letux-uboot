e7b6a6717ae5f0b3496b10790ca2c941bb022e33
Jason Kridner <jkridner@beagleboard.org>
2010-05-20 06:14:01 -0500

    BeagleBoard: Added LED driver
    Added LED driver using status_led.  USR0 is set to monitor the boot
    status.  USR1 is set to be the green LED.
    (cherry picked from commit 048b526fd7cc0c642f27c674b3e235321c880b66)

--- board/ti/beagle/beagle.c
+++ board/ti/beagle/beagle.c
@@ -30,6 +30,9 @@
  * MA 02111-1307 USA
  */
 #include <common.h>
+#ifdef CONFIG_STATUS_LED
+#include <status_led.h>
+#endif
 #include <twl4030.h>
 #include <asm/io.h>
 #include <asm/arch/mux.h>
@@ -82,6 +85,10 @@ int board_init(void)
 	gd->bd->bi_arch_number = MACH_TYPE_OMAP3_BEAGLE;
 	/* boot param addr */
 	gd->bd->bi_boot_params = (OMAP34XX_SDRC_CS0 + 0x100);
+
+#if defined(CONFIG_STATUS_LED) && defined(STATUS_LED_BOOT)
+	status_led_set (STATUS_LED_BOOT, STATUS_LED_ON);
+#endif
 
 	return 0;
 }
@@ -282,3 +289,4 @@ void set_muxconf_regs(void)
 {
 	MUX_BEAGLE();
 }
+
