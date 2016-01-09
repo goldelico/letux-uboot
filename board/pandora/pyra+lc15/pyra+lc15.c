edb65fdf4c0f654f1e28ce83b9896d7beacab579
H. Nikolaus Schaller <hns@goldelico.com>
2016-01-09 15:58:22 +0100

    Pyra: code cleanup for the Pyra specific pinmux settings
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- board/pandora/pyra+lc15/pyra+lc15.c
+++ board/pandora/pyra+lc15/pyra+lc15.c
@@ -12,12 +12,13 @@
 
 #undef sysinfo
 #undef set_muxconf_regs_essential
+#undef spl_start_uboot
 
 const struct omap_sysinfo sysinfo = {
 	"Board: Pyra with Letux Cortex 15\n"
 };
 
-const struct pad_conf_entry core_padconf_array_essential_alternate[] = {
+const struct pad_conf_entry core_padconf_array_essential_pyra[] = {
 
 	{EMMC_CLK, (PTU | IEN | M0)}, /*  EMMC_CLK   */
 	{EMMC_CMD, (PTU | IEN | M0)}, /*  EMMC_CMD   */
@@ -35,10 +36,8 @@ const struct pad_conf_entry core_padconf_array_essential_alternate[] = {
 	{SDCARD_DATA1, (PTU | IEN | M0)}, /*  SDCARD_DATA1*/
 	{SDCARD_DATA2, (PTU | IEN | M0)}, /*  SDCARD_DATA2*/
 	{SDCARD_DATA3, (PTU | IEN | M0)}, /*  SDCARD_DATA3*/
-	{UART3_RX_IRRX, (PTU | M4)}, /*  SDIO4_CLK */
-	{UART3_TX_IRTX, (PTU | M4)},  /*  SDIO4_CMD */
-	{RFBI_DATA1, (PTU | IEN | M4)},  /*  UART3_RX_IRRX */
-	{RFBI_DATA2, (M4)}, /*  UART3_TX_IRTX */
+//	{UART3_RX_IRRX, (PTU | IEN | M0)}, /*  UART3_RX_IRRX    */
+//	{UART3_TX_IRTX, (M0)},    /*  UART3_TX_IRTX    */
 	{USBB1_HSIC_STROBE, (PTU | IEN | M0)},    /*  USBB1_HSIC_STROBE */
 	{USBB1_HSIC_DATA, (PTU | IEN | M0)},    /*  USBB1_HSIC_DATA */
 	{USBB2_HSIC_STROBE, (PTU | IEN | M0)},    /*  USBB2_HSIC_STROBE */
@@ -52,13 +51,27 @@ const struct pad_conf_entry core_padconf_array_essential_alternate[] = {
 	{I2C5_SDA, (IEN | M0)}, /* I2C5_SDA */
 	{HSI2_ACWAKE, (PTU | M6)},    /*  HSI2_ACWAKE */
 	{HSI2_CAFLAG, (PTU | M6)},    /*  HSI2_CAFLAG */
+	// switch UART3 to different pins
+	{UART3_RX_IRRX, (PTU | M4)}, /*  SDIO4_CLK */
+	{UART3_TX_IRTX, (PTU | M4)},  /*  SDIO4_CMD */
+	{RFBI_DATA1, (PTU | IEN | M4)},  /*  UART3_RX_IRRX */
+	{RFBI_DATA2, (M4)}, /*  UART3_TX_IRTX */
+	// enable I2C1..5 pullups
+	{I2C1_SCL, (PTU | IEN | M0)}, /* I2C1_SCL */
+	{I2C1_SDA, (PTU | IEN | M0)}, /* I2C1_SDA */
+	{I2C2_SCL, (PTU | IEN | M0)}, /* I2C2_SCL */
+	{I2C2_SDA, (PTU | IEN | M0)}, /* I2C2_SDA */
+	{I2C3_SCL, (PTU | IEN | M0)}, /* I2C3_SCL */
+	{I2C3_SDA, (PTU | IEN | M0)}, /* I2C3_SDA */
+	{I2C4_SCL, (PTU | IEN | M0)}, /* I2C4_SCL */
+	{I2C4_SDA, (PTU | IEN | M0)}, /* I2C4_SDA */
 };
 
 void set_muxconf_regs_essential(void)
 {
 	do_set_mux((*ctrl)->control_padconf_core_base,
-		   core_padconf_array_essential_alternate,
-		   sizeof(core_padconf_array_essential_alternate) /
+		   core_padconf_array_essential_pyra,
+		   sizeof(core_padconf_array_essential_pyra) /
 		   sizeof(struct pad_conf_entry));
 
 	do_set_mux((*ctrl)->control_padconf_wkup_base,
