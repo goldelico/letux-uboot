f36d0bc4851de0a09b22cd3b59aff84abb1b25f5
H. Nikolaus Schaller <hns@goldelico.com>
2020-12-05 08:27:41 +0100

    tools/image-host.c: add support for clock_gettime() on Darwin host
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- tools/image-host.c
+++ tools/image-host.c
@@ -14,6 +14,23 @@
 #include <image.h>
 #include <version.h>
 
+/* as in https://stackoverflow.com/a/9781275 */
+#if defined(__MACH__) && !defined(CLOCK_REALTIME)
+#include <sys/time.h>
+#define CLOCK_REALTIME 0
+// clock_gettime is not implemented on older versions of OS X (< 10.12).
+// If implemented, CLOCK_REALTIME will have already been defined.
+int clock_gettime(int clk_id, struct timespec* t) {
+    struct timeval now;
+    int rv = gettimeofday(&now, NULL);
+    if (rv) return rv;
+    t->tv_sec  = now.tv_sec;
+    t->tv_nsec = now.tv_usec * 1000;
+    return 0;
+}
+#define CLOCK_MONOTONIC 1
+#endif
+
 /**
  * fit_set_hash_value - set hash value in requested has node
  * @fit: pointer to the FIT format image header
