a377e0f085a8dce35158834bc3e8b5e42656d9ae
H. Nikolaus Schaller <hns@goldelico.com>
2025-01-27 17:28:52 +0100

    halley6: set CONFIG_BOOTCOMMAND to boot lx16.dtb
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- include/configs/halley6.h
+++ include/configs/halley6.h
@@ -158,7 +158,7 @@
 	#define CONFIG_BOOTCOMMAND "tftpboot 0x80600000 user/pzqi/uImage; bootm 0x80600000"
 /*#define CONFIG_BOOTCOMMAND "loady 0x80600000; bootm 0x80600000"*/
 #elif defined(CONFIG_SPL_JZMMC_SUPPORT) || defined(CONFIG_SPL_MMC_SUPPORT)
-	#define CONFIG_BOOTCOMMAND "fatload mmc 0 0x80a00000 /uImage; bootm 0x80a00000"
+	#define CONFIG_BOOTCOMMAND "fatload mmc 0 0x81f00000 /ingenic/lx16.dtb; fatload mmc 0 0x80a00000 /uImage; bootm 0x80a00000 - 0x81f00000"
 #elif defined(CONFIG_SPL_SFC_NOR)
 	#define CONFIG_BOOTCOMMAND "sfcnor read 0x40000 0x600000 0x80a00000 ;bootm 0x80a00000"
 #elif defined(CONFIG_SPL_SFC_NAND)
