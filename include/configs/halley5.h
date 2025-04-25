d29af0de80028bf52c1445b1cb9cdda312f5abb4
H. Nikolaus Schaller <hns@goldelico.com>
2025-04-25 13:48:16 +0200

    halley5: configure for mtest and define memory end
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- include/configs/halley5.h
+++ include/configs/halley5.h
@@ -514,6 +514,13 @@
 #define CONFIG_CMD_LOADB	/* loadb			*/
 #define CONFIG_CMD_LOADS	/* loads			*/
 #define CONFIG_CMD_MEMORY	/* md mm nm mw cp cmp crc base loop mtest */
+#if 1
+#define CONFIG_LOOPW
+#define CONFIG_CMD_MEMTEST
+#endif
+#if 0
+#define CONFIG_SYS_ALT_MEMTEST
+#endif
 #define CONFIG_CMD_MISC		/* Misc functions like sleep etc*/
 #define CONFIG_CMD_NET		/* networking support			*/
 #define CONFIG_CMD_PING
@@ -568,7 +575,7 @@
 #define CONFIG_SYS_INIT_SP_OFFSET	0x400000
 #define CONFIG_SYS_LOAD_ADDR		0x88000000
 #define CONFIG_SYS_MEMTEST_START	0x80000000
-#define CONFIG_SYS_MEMTEST_END		0x88000000
+#define CONFIG_SYS_MEMTEST_END		0x86000000
 
 #define CONFIG_SYS_TEXT_BASE		0x80100000
 #define CONFIG_SYS_MONITOR_BASE		CONFIG_SYS_TEXT_BASE
