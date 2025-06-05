b30cb5ea0af831810b2150b9c2685458c8fbf85e
H. Nikolaus Schaller <hns@goldelico.com>
2025-06-05 16:13:30 +0200

    halley5: add ieee754=emulated to BOOTARGS_COMMON so that we can use Debian mipsel rootfs
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- include/configs/halley5.h
+++ include/configs/halley5.h
@@ -136,9 +136,9 @@
 #define CONFIG_BOOTARGS_AUTO_MODIFY	1	/*auto detect memory size, and modify bootargs for kernel.*/
 
 #if (CONFIG_BOOTARGS_AUTO_MODIFY == 1)
-	#define BOOTARGS_COMMON LINUX_LOGLEVEL " console=ttyS2,115200 "
+	#define BOOTARGS_COMMON LINUX_LOGLEVEL " ieee754=emulated console=ttyS2,115200 "
 #else
-	#define BOOTARGS_COMMON LINUX_LOGLEVEL " console=ttyS2,115200 mem=128M@0x0 "
+	#define BOOTARGS_COMMON LINUX_LOGLEVEL " ieee754=emulated console=ttyS2,115200 mem=128M@0x0 "
 #endif
 
 
